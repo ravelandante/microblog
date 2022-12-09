4038b95c8f2d3630c4ab3df2b144c57c6422bd77
MORPHEUS\Fynn <fynn@fynnyoung.com>
2022-12-09 11:36:34 +0200

    add logging in + out

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
-from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
+from app.models import User
+from flask import render_template, flash, redirect, url_for
+from flask_login import current_user, login_user, logout_user
 
 # index/home page
 @app.route('/')
@@ -25,12 +27,22 @@ def index():
 # login page
 @app.route('/login', methods=['GET', 'POST'])   # allow POST (submitting of data)
 def login():
+    # if user is already logged in and navigates to /login
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        # flash login message
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        # return to index
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            # flash error message
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
         return redirect(url_for('index'))
-    # render login if 1st time loading or validation failed
+    # render login if 1st time loading
     return render_template('login.html',  title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
